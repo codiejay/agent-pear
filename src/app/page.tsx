54b61249778073a38de47ba08888aaeaf07946fe
James Akpan <jamesakpan016@gmail.com>
2025-07-04 01:21:08 +0100

    Fix: Add suspect to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { Suspense } from "react";
 import { MainLayout } from "@/features/layout/components/MainLayout";
 import { SignalList } from "@/features/signals/components/SignalList";
 
 export default function Home() {
   return (
-    <MainLayout>
-      <div>
-        <SignalList />
-      </div>
-    </MainLayout>
+    <Suspense fallback={<div>Loading...</div>}>
+      <MainLayout>
+        <div>
+          <SignalList />
+        </div>
+      </MainLayout>
+    </Suspense>
   );
 }
